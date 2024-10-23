be854f99874486506b66878fc837d38f04c29118
annyC31 <0000110755696xsp@al.educacao.sp.gov.br>
2024-10-23 16:47:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da fraça?',
+    'A capital da fraça é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI'
+)
